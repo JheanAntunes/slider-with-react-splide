9e7dffc8887722d8942717025ea9f926a3ff2936
jhean <brizadaojhean@gmail.com>
2023-12-16 19:45:56 -0300

    :lipstick: feat: melhorando a estilização do card simulation image.

--- src/components/utils/sliders.tsx
+++ src/components/utils/sliders.tsx
@@ -1,4 +1,5 @@
 import { SpliderSliderClient } from '@/components/example/splider-slider-client'
+import { TypographyH2 } from '../typography/tipography'
 import { dataSliders } from './dataSliders'
 
 function Sliders() {
@@ -6,8 +7,10 @@ function Sliders() {
     <>
       {dataSliders.map((valueCurrent, index) => (
         <SpliderSliderClient key={valueCurrent + index}>
-          <div className="h-56 w-full rounded bg-emerald-600 text-3xl text-zinc-950">
-            {valueCurrent + 1}
+          <div className="flex h-56 w-full items-center justify-center rounded bg-emerald-600">
+            <TypographyH2 className="text-zinc-950 lg:text-3xl">
+              {valueCurrent + 1}
+            </TypographyH2>
           </div>
         </SpliderSliderClient>
       ))}
