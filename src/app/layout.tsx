2c5012732eb5c6d5546185835a93ef0cdc977f57
jhean <brizadaojhean@gmail.com>
2023-12-16 19:27:10 -0300

    :lipstick: feat: estilização do body gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={
+          inter.className +
+          ' bg-gradient-to-tl from-slate-900 via-slate-800 to-gray-900'
+        }
+      >
+        {children}
+      </body>
     </html>
   )
 }
