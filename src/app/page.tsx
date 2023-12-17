a1b6eb105a41d57fcc65128432cbcbcd880dabfa
jhean <brizadaojhean@gmail.com>
2023-12-16 23:07:40 -0300

    :truck:	chore: import HomePageSliderResponsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import HomePageSliderBasic from '@/components/slider/home-page-slider-basic'
 import HomePageSliderAutoScroll from '@/components/slider/slider-auto-scroll/home-page-slider-auto-scroll'
+import HomePageSliderResponsive from '@/components/slider/slider-responsive/home-page-slider-responsive'
 import HomePageThumbnails from '@/components/slider/thumbnails/home-page-thumbnails'
 export default function Home() {
   return (
-    <main className="container flex flex-col gap-20">
+    <main className="container flex flex-col gap-8 pb-8">
       <HomePageSliderBasic />
       <HomePageSliderAutoScroll />
       <HomePageThumbnails />
+      <HomePageSliderResponsive />
     </main>
   )
 }
