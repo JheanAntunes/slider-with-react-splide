5f8cf5f9bc6f527f9cb6b2a4eedfe0ff32df8ecb
jhean <brizadaojhean@gmail.com>
2023-12-16 23:06:44 -0300

    :sparkles:	feat: as options do carrousel slider-responsive-options

---
+++ src/components/slider/slider-responsive/slider-responsive-options.ts
@@ -0,0 +1,25 @@
+import { Options } from '@splidejs/react-splide'
+
+export const sliderResponsiveOptionsDefault: Options = {
+  mediaQuery: 'min', //mobile firts
+  breakpoints: {
+    1024: {
+      perPage: 3,
+      perMove: 1 // vai mover em quantos elementos no slider
+    },
+    768: {
+      perPage: 2, // A qtd que o slider vai mostrar
+      perMove: 1 // vai mover em quantos elementos no slider
+    }
+  },
+  type: 'loop', //carrosel infinito.
+  snap: true, // ajeita o próximo sem rodar.
+  rewind: true, // a velocidade de transition
+  start: 0, //O slider se inicia
+  drag: 'free', //não permite o usuario arrastar o slider mobile
+  perPage: 1, // A qtd que o slider vai mostrar
+  perMove: 1, // vai mover em quantos elementos no slider
+  gap: '2rem'
+}
+// export const sliderResponsiveOptions = (options: Options): Options =>
+//   sliderResponsiveOptionsDefault
