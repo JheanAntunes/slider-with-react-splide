a5f80c76fe14061dfbed8451d40bc0438f5860fe
jhean <brizadaojhean@gmail.com>
2023-12-16 21:52:36 -0300

    :truck:	chore: imports HomePageSliderAutoScroll & HomePageThumbnails

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import HomePageSliderBasic from '@/components/slider/home-page-slider-basic'
+import HomePageSliderAutoScroll from '@/components/slider/slider-auto-scroll/home-page-slider-auto-scroll'
+import HomePageThumbnails from '@/components/slider/thumbnails/home-page-thumbnails'
 export default function Home() {
   return (
     <main className="container flex flex-col gap-20">
       <HomePageSliderBasic />
+      <HomePageSliderAutoScroll />
+      <HomePageThumbnails />
     </main>
   )
 }
