d6a07619d1b0cc9bb1832f96979e3e91859072e3
jhean <brizadaojhean@gmail.com>
2023-12-16 21:41:31 -0300

    :sparkles:	feat: component shorthand para slider-auto-scroll

---
+++ src/components/slider/slider-auto-scroll/splide-client-auto-scroll.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { SplideClient } from '@/components/example/splider-slider-client'
+import { Options, SplideProps } from '@splidejs/react-splide'
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll'
+import { PropsWithChildren } from 'react'
+
+type SplideClientAutoScrollProps = {
+  options: Options
+} & SplideProps &
+  PropsWithChildren
+
+function SplideClientAutoScroll({
+  options,
+  children,
+  ...restProps
+}: SplideClientAutoScrollProps) {
+  return (
+    <SplideClient
+      options={{
+        ...options,
+        autoScroll: {
+          speed: 0.5,
+          pauseOnHover: false,
+          pauseOnFocus: false,
+          rewind: false
+        }
+      }}
+      extensions={{ AutoScroll }}
+      {...restProps}
+    >
+      {children}
+    </SplideClient>
+  )
+}
+
+export default SplideClientAutoScroll
