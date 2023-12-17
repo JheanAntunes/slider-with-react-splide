c66f644b75b2115b7de8142608ce7fc707f64c25
jhean <brizadaojhean@gmail.com>
2023-12-17 20:48:15 -0300

    :bug: fix: tentando declarar module em novo ts global.

---
+++ splider.d.ts
@@ -0,0 +1,8 @@
+declare module "@splidejs/splide" {
+  interface Options {
+    autoScroll?: AutoScrollOptions | boolean;
+  }
+  interface Components {
+    AutoScroll?: AutoScrollComponent;
+  }
+}
