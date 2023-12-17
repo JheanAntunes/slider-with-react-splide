844d4f48a3c1bfb67ea66523c51f803d763374b6
jhean <brizadaojhean@gmail.com>
2023-12-16 21:38:36 -0300

    :sparkles:	feat: atribuindo o pattern cn

--- src/components/typography/tipography.tsx
+++ src/components/typography/tipography.tsx
@@ -1,9 +1,11 @@
+import { cn } from '../utils/cn'
+
 export const TypographyH1 = ({
   className,
   children,
   ...restProps
 }: React.ComponentProps<'h1'>) => (
-  <h1 className={className} {...restProps}>
+  <h1 className={cn('text-2xl lg:text-4xl', className)} {...restProps}>
     {children}
   </h1>
 )
@@ -13,7 +15,7 @@ export const TypographyH2 = ({
   children,
   ...restProps
 }: React.ComponentProps<'h2'>) => (
-  <h2 className={className} {...restProps}>
+  <h2 className={cn('text-xl lg:text-3xl', className)} {...restProps}>
     {children}
   </h2>
 )
--- src/components/utils/sliders.tsx
+++ src/components/utils/sliders.tsx
@@ -1,14 +1,21 @@
 import { SpliderSliderClient } from '@/components/example/splider-slider-client'
 import { TypographyH2 } from '../typography/tipography'
+import { cn } from './cn'
 import { dataSliders } from './dataSliders'
 
-function Sliders() {
+function Sliders({ className, ...restProps }: React.ComponentProps<'div'>) {
   return (
     <>
       {dataSliders.map((valueCurrent, index) => (
         <SpliderSliderClient key={valueCurrent + index}>
-          <div className="flex h-56 w-full items-center justify-center rounded bg-emerald-600">
-            <TypographyH2 className="text-zinc-950 lg:text-3xl">
+          <div
+            className={cn(
+              'flex h-56 w-full items-center justify-center rounded bg-gradient-to-tr from-emerald-300 via-emerald-400 to-emerald-600',
+              className
+            )}
+            {...restProps}
+          >
+            <TypographyH2 className="font-mono font-bold text-blue-600 lg:text-3xl">
               {valueCurrent + 1}
             </TypographyH2>
           </div>
