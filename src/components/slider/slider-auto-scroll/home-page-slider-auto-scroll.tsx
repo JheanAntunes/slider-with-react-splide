6de4da14a33f964efa3fb8a7cb47c048aaae2371
jhean <brizadaojhean@gmail.com>
2023-12-16 21:41:39 -0300

    :sparkles:	feat: example-home-page-slider-auto-scroll finalizado.

---
+++ src/components/slider/slider-auto-scroll/home-page-slider-auto-scroll.tsx
@@ -0,0 +1,28 @@
+import { TypographyH1 } from '@/components/typography/tipography'
+import Sliders from '@/components/utils/sliders'
+import '@splidejs/react-splide/css'
+import SplideClientAutoScroll from './splide-client-auto-scroll'
+
+function HomePageSliderAutoScroll() {
+  return (
+    <section className="flex flex-col gap-5">
+      <TypographyH1 id="descriptionCarroselAutoScroll" className="lg:text-4xl">
+        Exemplo de carrosel automatico
+      </TypographyH1>
+      <SplideClientAutoScroll
+        options={{
+          type: 'loop',
+          perPage: 3,
+          perMove: 3,
+          drag: 'free',
+          gap: '1.25rem'
+        }}
+        aria-labelledby="descriptionCarroselAutoScroll"
+      >
+        <Sliders />
+      </SplideClientAutoScroll>
+    </section>
+  )
+}
+
+export default HomePageSliderAutoScroll
